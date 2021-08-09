42de34efc4c18d1d2404005e08622d9f9d67f9b7
Ken Nguyen <kennj.consultant@gmail.com>
2021-08-09 05:30:14 -0400

    Fix: add another variable

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -16,6 +16,8 @@ const About = () => {
     useEffect(() => {
         let str = 'I do not want delete it and add str var';
         let mount = false;
+
+        let newStr = 'another variable';
         const resizeHandle = () => {
             setSum(122);
             console.dir(spanRef.current.innerHTML = '123');
