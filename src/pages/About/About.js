e775c190887d165a7b7b53f1827c4b29db341eab
Ken Nguyen <kennj.consultant@gmail.com>
2021-08-09 05:09:50 -0400

    Feature: Add about page

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const About = () => {
+    const [sum, setSum] = useState(0);
+    const spanRef = useRef(false);
+
+    const handlePlus = () => {
+        setTimeout(() => {
+            setSum((preSum) => {
+                let temp = preSum + 1;
+                return temp;
+            });
+        }, 5000);
+    }
+
+    useEffect(() => {
+        let mount = true;
+        const resizeHandle = () => {
+            setSum(122);
+            console.dir(spanRef.current.innerHTML = '123');
+        }
+        window.addEventListener('resize', resizeHandle)
+        return () => {
+            window.removeEventListener('resize', resizeHandle)
+        };
+    }, [])
+
     return (
         <div className="about">
-            About content
+            SUM: {sum}
+            <button onClick={handlePlus}>Plus</button>
+            <span ref={spanRef}></span>
         </div>
     );
 };
